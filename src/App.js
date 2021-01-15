bad94ae772b186f0e3e6ce3c2512d3e37fe48d5c
Helena Johansson <helena@klarr.se>
2021-01-15 10:52:35 +0100

    starting code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Words from "./components/Words";
+
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [word, setWord] = useState(Words);
+  const [newWord, setNewWord] = useState(word[0]);
+  // first word in words array is initial state value
+  const [disabled, setDisabled] = useState(true);
+  const [currentResults, setCurrentResults] = useState([]);
+  // display current result
+  const [wrongResults, setWrongResults] = useState([]);
+  const [countCurrent, setCountCurrent] = useState(0);
+  const [time, setTime] = useState(30);
+  const [inputValue, setInputValue] = useState("");
+  // state for our input
+  const [animation, setAnimation] = useState(null);
+
+  return <div className="typing-game">Typing Game</div>;
 }
 
 export default App;
