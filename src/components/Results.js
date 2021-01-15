ca8dff6531eb3a695a7a787ca017c9b072f302fc
Helena Johansson <helena@klarr.se>
2021-01-15 12:54:26 +0100

    results finished

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,7 +4,29 @@ const Results = ({ correctResults, wrongResults, countCorrect }) => {
   return (
     <div className="results">
       <div className="title">
-        <p>Correct answers:</p>
+        <p>Correct answers: {countCorrect}</p>
+      </div>
+
+      <div className="resultsContainer">
+        <div className="correctResults">
+          <p>
+            {correctResults.map((correctWord, index) => (
+              <div className="row" key={index}>
+                <p>{correctWord}</p>
+              </div>
+            ))}
+          </p>
+        </div>
+
+        <div className="wrongREsults">
+          <p>
+            {wrongResults.map((wrongWord, index) => (
+              <div className="row" key={index}>
+                <p>{wrongWord}</p>
+              </div>
+            ))}
+          </p>
+        </div>
       </div>
     </div>
   );
